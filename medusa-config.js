6823ad244ce0a7050695f29d128161d450862a87
computadorescol <computadorescol@gmail.com>
2026-09-06 20:59:47 -0500

    delete old.medusa-config.ts y delete admin in medusa.config,js

--- medusa-config.js
+++ medusa-config.js
@@ -1,26 +1,17 @@
- const { loadEnv, defineConfig } = require('@medusajs/framework/utils');
+ const { loadEnv, defineConfig } = require('@medusajs/framework/utils')
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    http: {
+    http: {       
       storeCors: process.env.STORE_CORS || '*',
       adminCors: process.env.ADMIN_CORS || '*',
       authCors: process.env.AUTH_CORS || '*',
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
     },
-    admin: {
-      vite: (config) => {
-        return {
-          server: {
-            host: '0.0.0.0',
-            allowedHosts: ['localhost', '.localhost', '127.0.0.1'],
-          },
-        };
-      },
-    },
+    
   },
 });
